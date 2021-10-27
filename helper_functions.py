731e2e145d490806ed9f9eca99cbcc90eaaed60b
Jake <jtyo@andrew.cmu.edu>
2021-10-27 17:38:13 -0400

    getting errors.

--- helper_functions.py
+++ helper_functions.py
@@ -29,8 +29,12 @@ def load_glove_model(fp):
         for line in f:
             split_line = line.split()
             word = split_line[0]
-            embedding = np.array(split_line[1:], dtype=np.float64)
-            glove_model[word] = embedding
+            try:
+                embedding = np.array(split_line[1:], dtype=np.float64)
+                glove_model[word] = embedding
+            except:
+                print(f'dropping the embedding for: {word}')
+    
     print(f"{len(glove_model)} words loaded!")
     return glove_model
 
